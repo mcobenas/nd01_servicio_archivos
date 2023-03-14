10e34dcd3a82ca646ebd22f36aa063e9cc5e09e3
Marco Tulio Cobeñas Vega <mcobenas@gmail.com>
2023-03-14 17:22:24 -0500

    commit 3

--- src/app.js
+++ src/app.js
@@ -2,6 +2,11 @@ import express  from "express";
 import personasRoutes from "./routes/personas.routes.js"
 
 const app=express();
+
+//se adiciona algunos middlewares
+app.use(express.json())  //este sirve para interpretar las cadenas json
+
+//se adiciona rutas
 app.use(personasRoutes)
 
 export default app
--- src/controllers/personas.controllers.js
+++ src/controllers/personas.controllers.js
@@ -1,8 +1,26 @@
 
+import {Persona} from "../models/Persona.js"
+
 export const getPersonas = (req,res) => {
     res.send("getting personas")
 }
 
-export const createPersonas = (req,res) => {
+export const createPersonas = async (req,res) => {
+    //console.log(req.body)
+    const { Nombre,ApelliPaterno,ApelliMaterno,
+        FechRegistro,UsuarioRegistro,MaquinaRegistro,IPRegistro}=req.body
+
+    const NuevaPersona= await Persona.create({
+        Nombre,
+        ApelliPaterno,
+        ApelliMaterno,
+        FechRegistro,
+        UsuarioRegistro,
+        MaquinaRegistro,
+        IPRegistro
+    })
+
+    console.log(NuevaPersona)
+
     res.send("creatting personas")
 }
